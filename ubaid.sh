49d63b222fa38525c186bf941d86bfe7197eef07
ubaidurrehman12345 <151608210+ubaidurrehman12345@users.noreply.github.com>
2023-12-12 00:28:18 +0500

    Update ubaid.sh

--- ubaid.sh
+++ ubaid.sh
@@ -1,2 +1,10 @@
+venvname="ubaidv"
+
+if [ ! -d "$venvname" ]; then
+    python -m venv "$venvname"
+fi
+
+source "$venvname/bin/activate"
+
 pip install flask gunicorn
 gunicorn -w 4 -b 0.0.0.0:1200 --reload ubaid:app
